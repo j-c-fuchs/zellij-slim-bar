85ccb72885e42a7ef572f8db691ff793c249c6bc
J. C. Fuchs <j-c-fuchs.git@pm.me>
2023-06-27 22:30:37 +0200

    Add dummy plugin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use zellij_tile::prelude::*;
+
+#[derive(Default)]
+struct SlimBar {}
+
+register_plugin!(SlimBar);
+
+impl ZellijPlugin for SlimBar {
+    fn load(&mut self) {
+        set_selectable(false);
+        subscribe(&[
+            EventType::TabUpdate,
+            EventType::ModeUpdate,
+            EventType::Mouse,
+        ]);
+    }
+
+    fn update(&mut self, event: Event) -> bool {
+        false
+    }
+
+    fn render(&mut self, rows: usize, cols: usize) {
+        return;
+    }
 }
