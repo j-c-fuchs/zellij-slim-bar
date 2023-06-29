3a3487c6fdb950a8ed2c9ef8a04a2805a8dee3fd
J. C. Fuchs <j-c-fuchs.git@pm.me>
2023-06-29 19:21:21 +0200

    Copy stuff from compact bar

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
+mod line;
+mod tab;
+
 use std::cmp::{max, min};
+use std::convert::TryInto;
 
 use zellij_tile::prelude::*;
+
+use crate::line::tab_line;
+use crate::tab::tab_style;
+
+#[derive(Debug, Default)]
+pub struct LinePart {
+    part: String,
+    len: usize,
+    tab_index: Option<usize>,
+}
 
 #[derive(Default)]
 struct SlimBar {
@@ -10,6 +24,8 @@ struct SlimBar {
     mouse_click_pos: usize,
     should_change_tab: bool,
 }
+
+static ARROW_SEPARATOR: &str = "";
 
 register_plugin!(SlimBar);
 
@@ -70,7 +86,77 @@ impl ZellijPlugin for SlimBar {
         should_render
     }
 
-    fn render(&mut self, rows: usize, cols: usize) {
-        return;
+    fn render(&mut self, _rows: usize, cols: usize) {
+        if self.tabs.is_empty() {
+            return;
+        }
+        let mut all_tabs: Vec<LinePart> = vec![];
+        let mut active_tab_index = 0;
+        let mut active_swap_layout_name = None;
+        let mut is_swap_layout_dirty = false;
+        let mut is_alternate_tab = false;
+        for t in &mut self.tabs {
+            let mut tabname = t.name.clone();
+            if t.active && self.mode_info.mode == InputMode::RenameTab {
+                if tabname.is_empty() {
+                    tabname = String::from("Enter name...");
+                }
+                active_tab_index = t.position;
+            } else if t.active {
+                active_tab_index = t.position;
+                is_swap_layout_dirty = t.is_swap_layout_dirty;
+                active_swap_layout_name = t.active_swap_layout_name.clone();
+            }
+            let tab = tab_style(
+                tabname,
+                t,
+                is_alternate_tab,
+                self.mode_info.style.colors,
+                self.mode_info.capabilities,
+            );
+            is_alternate_tab = !is_alternate_tab;
+            all_tabs.push(tab);
+        }
+        let tab_line = tab_line(
+            self.mode_info.session_name.as_deref(),
+            all_tabs,
+            active_tab_index,
+            cols.saturating_sub(1),
+            self.mode_info.style.colors,
+            self.mode_info.capabilities,
+            self.mode_info.style.hide_session_name,
+            self.mode_info.mode,
+            &active_swap_layout_name,
+            is_swap_layout_dirty,
+        );
+        let mut s = String::new();
+        let mut len_cnt = 0;
+        for bar_part in tab_line {
+            s = format!("{}{}", s, &bar_part.part);
+
+            if self.should_change_tab
+                && self.mouse_click_pos >= len_cnt
+                && self.mouse_click_pos < len_cnt + bar_part.len
+                && bar_part.tab_index.is_some()
+            {
+                // Tabs are indexed starting from 1, therefore we need add 1 to tab_index.
+                let tab_index: u32 = bar_part.tab_index.unwrap().try_into().unwrap();
+                switch_tab_to(tab_index + 1);
+            }
+            len_cnt += bar_part.len;
+        }
+        let background = match self.mode_info.style.colors.theme_hue {
+            ThemeHue::Dark => self.mode_info.style.colors.black,
+            ThemeHue::Light => self.mode_info.style.colors.white,
+        };
+        match background {
+            PaletteColor::Rgb((r, g, b)) => {
+                print!("{}\u{1b}[48;2;{};{};{}m\u{1b}[0K", s, r, g, b);
+            },
+            PaletteColor::EightBit(color) => {
+                print!("{}\u{1b}[48;5;{}m\u{1b}[0K", s, color);
+            },
+        }
+        self.should_change_tab = false;
     }
 }
